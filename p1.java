b3d0bd3ee71ce95b52bd9771fc3cb53fd4bb05e8
AkrutiPurohit <141224622+AkrutiPurohit@users.noreply.github.com>
2024-01-30 11:10:59 +0530

    Update p1.java

--- p1.java
+++ p1.java
@@ -1,9 +1,13 @@
+//Write a Java program that declares three integer variables (a, b, c) and compute their sum and average. 
+
 import java.util.Scanner;
 
 class p1
 {
 	public static void main(String s[])
 	{
+		System.out.println("220130318033");
+		System.out.println("Purohit Akruti k.");
 		int a,b,c,sum;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the number:");
